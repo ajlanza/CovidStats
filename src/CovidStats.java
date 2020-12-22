ddb7c28ad3e664fc187ef5315e86fc1c6f65708c
ajlanza <61472953+ajlanza@users.noreply.github.com>
2020-12-22 18:06:45 -0500

    set up to read data from txt file

---
+++ src/CovidStats.java
@@ -0,0 +1,25 @@
+import java.io.*;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.util.stream.Stream;
+
+public class CovidStats {
+    public static void main(String[] args) {
+        int[] covid19 = null;
+        try {
+            // read the data from the file
+            Path path = Paths.get("data.txt");
+            Stream<String> lines = Files.lines(path);
+
+            //convert to array of int
+            covid19 = lines.mapToInt(Integer::parseInt).toArray();
+
+        } catch(IOException e) {
+            e.printStackTrace();
+        }
+
+        System.out.println(covid19.length);
+        
+    }
+}
