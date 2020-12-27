9f114743a30d88d1569243aafbd1fae3a97f76d9
ajlanza <61472953+ajlanza@users.noreply.github.com>
2020-12-26 21:09:32 -0500

    added days since cases doubled last

--- src/CovidStats.java
+++ src/CovidStats.java
@@ -19,7 +19,42 @@ public class CovidStats {
             e.printStackTrace();
         }
 
-        System.out.println(covid19.length);
+        int[] dailyCaseCount = new int[covid19.length];
+        int previousDayValue = 0;
+        for(int i = 0; i < covid19.length; i++){
+          int dailyIncrease = covid19[i] - previousDayValue;
+          dailyCaseCount[i] = dailyIncrease;
+          previousDayValue = covid19[i];
+        }
+        for(int n: dailyCaseCount){
+          System.out.println(n);
+        }
+        System.out.println(dailyCaseCount.length);
+
+        int sum = 0;
+        for( int n : dailyCaseCount ){
+          sum = sum + n;
+        }
+        double average = (double)sum / (double)dailyCaseCount.length;
+        System.out.printf("Average new cases per day %.2f\n", average);
+
+        
+        // How many days has it been since the number of cases doubled??
+        int totalCount = covid19[covid19.length -1];
+        int halfCount = totalCount / 2;
+
+        int halfCountIndex = -1;
+        int i = 0;
+        while( i < covid19.length && halfCountIndex == -1){
+          if( covid19[i] > halfCount) {
+            halfCountIndex = i - 1;
+          }
+          i = i + 1;
+        }
+
+        int numDaysSinceDouble = (covid19.length - 1) - halfCountIndex;
+        System.out.printf("The number of days since the number of cases doubled is: %d\n", numDaysSinceDouble);
+
         
     }
 }
